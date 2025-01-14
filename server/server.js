d657e8e1ebb92ece462ca1f4013b5d78849de59c
aRonnieAlsop <alsop.arron@gmail.com>
2025-01-13 22:39:27 -0800

    'wip'

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use('/api', blogs);
 app.use('/api', projectRoutes);  
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
