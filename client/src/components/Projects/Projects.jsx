5a57a5c520645c767d0ff42d582dc001f15dd23c
aRonnieAlsop <alsop.arron@gmail.com>
2025-01-13 21:45:10 -0800

    'wip'

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -10,12 +10,14 @@ const Projects = () => {
       name: 'Hello World Test', 
       description: 'A simple test project',
       githubUrl: 'https://github.com/aRonnieAlsop/hello_world_test',
+      image_url: '../../assets/hello_world_test_deploy.png',
      },
     { 
-      id: 'tarot_game', 
-      name: 'Tarot Game', 
+      id: 'portfolio_website', 
+      name: 'Portfolio', 
       description: 'A more complex project but still a test project for the portfolio site.',
-      githubUrl: 'https://github.com/aRonnieAlsop/tarot_game',
+      githubUrl: 'https://github.com/aRonnieAlsop/portfolio_website',
+      image_url: '../../assets/faux_portfolio_sample.png',
     },
   ];
 
@@ -27,14 +29,15 @@ const Projects = () => {
       <div className="projects">
         {listProjects.map((project) => (
           <div key={project.id} className="projects-card">
+          <img src={project.image_url} alt={project.title} className="project-image" />
             <h2>{project.name}</h2> 
             <p>{project.description}</p> 
-            <Link to={`/projects/${project.id}`} className="check-it-out-button">
-              Check It Out
+            <Link className="demo-button">
+              Access the Demo
             </Link>
             {/* Link to the GitHub repository */}
-           <a href={project.githubUrl} target="_blank" rel="noopener noreferrer" className="github-button">
-              Look at the Code
+           <a href={project.githubUrl} target="_blank" rel="noopener noreferrer" className="repo-button">
+              Explore the Repository
             </a>
           </div>
         ))}
